25bf4c7bc529d7574f206f888bbb9e9b3ae7dfa6
Anthony Lapham <lapham.2@wright.edu>
2018-07-15 10:10:07 -0400

    updates

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 console.log("Starting app.js");
 
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash');
+
 const notes =require('./notes.js');
 
-var res = notes.addNote();
-console.log(res);
+var command = process.argv[2];
+console.log('Command: ', command);
+console.log(process.argv);
 
-var math = notes.addNumbers(5,10);
-console.log(math);
-
-// var user= os.userInfo();
-//
-// fs.appendFileSync('greetings.txt', `Hello ${user.username}!`)
+if(command === 'add'){
+  console.log('Adding new note');
+}else if(command === 'read'){
+  console.log('Reading notes');
+}else if(command === 'remove'){
+  console.log('Removing notes');
+}else if (command === 'list'){
+  console.log('Listing all notes');
+}else{
+  console.log('Command not recognized');
+}
--- notes.js
+++ notes.js
@@ -4,8 +4,3 @@ module.exports.addNote = ()  => {
   console.log('addNote');
   return 'New note';
 };
-
-module.exports.addNumbers = function (a,b){
-  console.log('addNumbers');
-  return a + b;
-}
