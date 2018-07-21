2959218423baad619a150c9ce5dcd319830bc06b
Anthony Lapham <lapham.2@wright.edu>
2018-07-21 11:26:57 -0400

    file sync

--- notes.js
+++ notes.js
@@ -1,7 +1,28 @@
 console.log('Starting notes.js')
 
+const fs = require('fs');
+
 var addNote = function(title, body) {
-  console.log('Adding note', title, body);
+  var notes = [];
+  var note = {
+    title: title,
+    body: body
+  };
+
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+  } catch (error) {
+
+  }
+
+  var duplicateNotes = notes.filter((note) => note.title === title);
+
+
+  if (duplicateNotes.lenght === 0) {
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }
 };
 
 var getAll = () => {
